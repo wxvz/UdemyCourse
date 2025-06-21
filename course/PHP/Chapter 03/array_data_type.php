1280cefb327c110f8f88f08a13a6da78998677e7
Frank Aka <157696799+wxvz@users.noreply.github.com>
2025-06-22 00:48:05 +0100

    Array data type in PHP

---
+++ course/PHP/Chapter 03/array_data_type.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Data Type</title>
+</head>
+<body>
+    <?php
+    $students =  array("all","frank","bobb","Alix");
+    echo $students[0];
+    echo "<br>";
+    echo var_dump($students);
+
+     
+    ?>
+</body>
+</html>
