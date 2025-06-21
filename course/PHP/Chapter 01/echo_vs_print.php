23d44db3b5aceb4a130f291646ed9704e9c26e0c
Frank Aka <157696799+wxvz@users.noreply.github.com>
2025-06-21 01:12:52 +0100

    echo vs print

---
+++ course/PHP/Chapter 01/echo_vs_print.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP echo vs print method</title>
+</head>
+<body>
+    <?php 
+    echo "<h1>My Code</h1>", " Today is Saturday";
+    echo "<br>";
+    print "Frank Aka";
+
+    $a = print(34+3);
+    echo($a);
+    ?>
+</body>
+</html>
