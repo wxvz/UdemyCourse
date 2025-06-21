cff4aa5dcf3f9727d73dddd9550e76ad79ed3ba1
Frank Aka <157696799+wxvz@users.noreply.github.com>
2025-06-21 01:30:58 +0100

    escape sequences in php

---
+++ course/PHP/Chapter 01/escape_sequence.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Escape Sequence</title>
+</head>
+<body>
+    <?php 
+    echo "<pre>
+    \$How \n are \" \\ \t you</pre>"
+    ?>
+</body>
+</html>
