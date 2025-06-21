b274097f634f27e4499a5b705484639a461e3039
Frank Aka <157696799+wxvz@users.noreply.github.com>
2025-06-21 01:19:11 +0100

    php comments

---
+++ course/PHP/Chapter 01/php_comments.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP comments</title>
+</head>
+<body>
+    <?php 
+    // This is a comment
+    # this is a comment
+    /*
+    This is a multi line comment
+    This is my code
+    */
+    echo "<h1>My Code</h1>", " Today is Saturday";
+    //This code is written by frank aka
+    echo "<br>";
+    print "Frank Aka";
+
+    $a = print(34+3);// adding two numbers
+    ?>
+</body>
+</html>
