0fdffad15122056b0bed82d455cbf6d90859670b
Frank Aka <157696799+wxvz@users.noreply.github.com>
2025-06-22 01:11:18 +0100

    null data type in php

---
+++ course/PHP/Chapter 03/null_data_type.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Data Type</title>
+</head>
+<body>
+    <?php
+    $name = "Frank Aka";
+    $age = 234;
+    $val = 123;
+    $val2 = null;
+    echo $val;
+    echo var_dump($val2);
+    ?>
+</body>
+</html>
