3be242ce0fb8e5d6b4644242da0910b8a8efe034
Frank Aka <157696799+wxvz@users.noreply.github.com>
2025-06-22 15:39:49 +0100

    string built-in methods in PHP

---
+++ course/PHP/Chapter 04/string_builtin_methods.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Built in Methods</title>
+</head>
+<body>
+    <?php
+    $str = "How are you guys, I'm learning PHP";
+    echo strlen($str) . "<br>"; // Length of the string
+    echo str_word_count($str) . "<br>"; // Word count in the string
+    echo strrev($str) . "<br>"; // Reverse the string
+    echo strpos($str, "PHP") . "<br>"; // Position of the first occurrence
+    echo str_replace("PHP", "Python", $str) . "<br>"; // Replace PHP with Python
+    ?> 
+</body>
+</html>
