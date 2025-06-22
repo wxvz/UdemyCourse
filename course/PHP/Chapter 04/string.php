77fa722d3abc3aa02ee93c545e038e1e27b61114
Frank Aka <157696799+wxvz@users.noreply.github.com>
2025-06-22 01:11:08 +0100

    string in php

---
+++ course/PHP/Chapter 04/string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Data Type</title>
+</head>
+<body>
+    <?php
+    $str1 = "Frank Aka";
+    $str2 = "Frank Aka";
+    $str3 = "My name is 'Frank' aka";
+    $str4 = 'My name is "Frank" aka';
+    $val1 = 123;
+    $str5 = "value is $val1";
+    echo $str5;
+    ?>
+</body>
+</html>
